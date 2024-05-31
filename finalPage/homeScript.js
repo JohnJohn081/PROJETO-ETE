c162982c92a6382608f143d75a10b87927907e19
John John <johnmaclebio@gmail.com>
2024-05-31 18:03:22 -0300

    versao test 1.2.1

--- finalPage/homeScript.js
+++ finalPage/homeScript.js
@@ -55,7 +55,7 @@ const loadRanking = () => {
             querySnapshot.forEach((doc, index) => {
                 let data = doc.data();
                 let name = data.name.length > 15 ? data.name.substring(0, 16) + '...' : data.name;
-                rankingList.innerHTML += `<h3> ${name} - ${data.class} | ${data.score}/3 Pontos</h3>`;
+                rankingList.innerHTML += `<h3> ${name} - ${data.class} | ${data.score}/180 Pontos</h3>`;
             });
         })
         .catch((error) => {
